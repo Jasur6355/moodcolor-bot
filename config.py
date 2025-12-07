aa6c486777293fbefc550b8b3a72ff4e35dad141
Jasurbek <83454158+Jasur6355@users.noreply.github.com>
2025-12-07 18:04:11 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
-BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-import os
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # Render will read this
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMP_DIR = os.path.join(BASE_DIR, "temp")
+
