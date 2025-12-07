69828a7716dd9b2484c454602690de78d569d8db
Jasurbek <83454158+Jasur6355@users.noreply.github.com>
2025-12-07 17:34:48 +0500

    Use environment variable for BOT_TOKEN
    Replace hardcoded BOT_TOKEN with environment variable.

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
-# config.py
+import os
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-BOT_TOKEN = "7407855832:AAGzwJtyAPllQpQH7nnydkYUdZjFPxFNtDw"
-
-# vaqtinchalik rasmlar saqlanadigan papka
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMP_DIR = os.path.join(BASE_DIR, "temp")
